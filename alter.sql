ab03d7793a1d4049395479f6a0744e5a47230d95
aayush-vyas <122258263+aayush-vyas@users.noreply.github.com>
2023-02-21 19:50:45 +0530

    Add files via upload

---
+++ alter.sql
@@ -0,0 +1,38 @@
+--  ADD COLUMN 
+
+ALTER TABLE user
+add gender varchar(50) default 'male';
+
+select *from user;
+
+
+
+-- drop column
+alter table user
+drop column gender;
+
+-- modfiy column
+alter table user
+modify gender varchar(20) default 'not know yet';
+
+
+alter table user
+modify user_id int auto_increment not null;
+
+select *from user;
+
+insert into user(user_id, name, email) values(10,'zaid', 'zaid@gmail.com');
+
+insert into user values(11, 'dhenish', null, 'male');
+
+alter table user 
+add constraint uq_email unique(email);
+
+set SQL_SAFE_UPDATES = 0;
+
+delete from user where email is null;
+
+select *from user;
+
+
+select table_name, constraint_type, constraint_name from information_schema.table_constraints  where table_name = "user";
